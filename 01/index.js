da35018cc8ab77e84bdc954f112f99d0be0ce01c
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-22 10:33:28 -0700

    My first NodeJS HTTP Server

---
+++ 01/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join( __dirname, 'views/layouts' )
+}))
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'N. Hernández'
+    })
+});
+
+app.set( 'view engine', '.hbs' );
+app.set( 'views', path.join(__dirname, 'views') );
+
+app.listen(3000);
